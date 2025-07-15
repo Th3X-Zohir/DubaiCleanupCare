6ee12a547701bb7726490d4b3e9ba08cf5201998
Zohir Rayhan <zohirrayhanweb@gmail.com>
2025-07-16 02:40:52 +0600

    gitconnect

---
+++ public/deploy.php
@@ -0,0 +1,14 @@
+<?php
+$secret = 'xyz123abc456'; // Your secure string
+$header = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
+$payload = file_get_contents('php://input');
+$hash = 'sha256=' . hash_hmac('sha256', $payload, $secret);
+if (hash_equals($hash, $header)) {
+    shell_exec('cd /home/dubaicleanupcare/public_html && /bin/git fetch origin main && /bin/git reset --hard origin/main');
+    http_response_code(200);
+    echo 'Deployment successful';
+} else {
+    http_response_code(403);
+    echo 'Invalid signature';
+}
+?>
