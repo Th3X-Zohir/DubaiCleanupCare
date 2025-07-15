b1d120f0b938331d84eb92b760ac7931698f91b5
Zohir Rayhan <zohirrayhanweb@gmail.com>
2025-07-16 03:27:57 +0600

    deployfile

--- public/deploy.php
+++ public/deploy.php
@@ -4,9 +4,9 @@ $header = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
 $payload = file_get_contents('php://input');
 $hash = 'sha256=' . hash_hmac('sha256', $payload, $secret);
 if (hash_equals($hash, $header)) {
-    file_put_contents('/home/dubaicleanupcare/public_html/.deploy_trigger', 'triggered');
+    shell_exec('cd /home/dubaicleanupcare/public_html && /bin/git fetch origin main && /bin/git reset --hard origin/main && /bin/chmod -R 755 /home/dubaicleanupcare/public_html && /bin/chown -R dubaicleanupcare:dubaicleanupcare /home/dubaicleanupcare/public_html && /usr/local/bin/php /home/dubaicleanupcare/public_html/artisan optimize:clear && /usr/local/bin/php /home/dubaicleanupcare/public_html/artisan config:cache && /usr/local/bin/php /home/dubaicleanupcare/public_html/artisan route:cache && /usr/local/bin/php /home/dubaicleanupcare/public_html/artisan view:cache');
     http_response_code(200);
-    echo 'Deployment triggered';
+    echo 'Deployment successful';
 } else {
     http_response_code(403);
     echo 'Invalid signature';
